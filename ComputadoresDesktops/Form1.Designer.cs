8d82ab69379a9f38f66b83a4d90f55159246417f
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-01 22:12:48 -0300

    Adicionar arquivos de projeto.

---
+++ ComputadoresDesktops/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace ComputadoresDesktops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxComputador2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxComputador1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNotebook1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxComputador3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNotebook2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNotebook3 = new System.Windows.Forms.PictureBox();
+            this.radioButtonComputador = new System.Windows.Forms.RadioButton();
+            this.radioButtonNotebook = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.buttonSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxComputador2
+            // 
+            this.pictureBoxComputador2.Location = new System.Drawing.Point(98, 188);
+            this.pictureBoxComputador2.Name = "pictureBoxComputador2";
+            this.pictureBoxComputador2.Size = new System.Drawing.Size(159, 103);
+            this.pictureBoxComputador2.TabIndex = 0;
+            this.pictureBoxComputador2.TabStop = false;
+            this.pictureBoxComputador2.Visible = false;
+            // 
+            // pictureBoxComputador1
+            // 
+            this.pictureBoxComputador1.Location = new System.Drawing.Point(98, 83);
+            this.pictureBoxComputador1.Name = "pictureBoxComputador1";
+            this.pictureBoxComputador1.Size = new System.Drawing.Size(159, 99);
+            this.pictureBoxComputador1.TabIndex = 1;
+            this.pictureBoxComputador1.TabStop = false;
+            this.pictureBoxComputador1.Visible = false;
+            // 
+            // pictureBoxNotebook1
+            // 
+            this.pictureBoxNotebook1.Location = new System.Drawing.Point(464, 83);
+            this.pictureBoxNotebook1.Name = "pictureBoxNotebook1";
+            this.pictureBoxNotebook1.Size = new System.Drawing.Size(160, 99);
+            this.pictureBoxNotebook1.TabIndex = 2;
+            this.pictureBoxNotebook1.TabStop = false;
+            this.pictureBoxNotebook1.Visible = false;
+            // 
+            // pictureBoxComputador3
+            // 
+            this.pictureBoxComputador3.Location = new System.Drawing.Point(98, 297);
+            this.pictureBoxComputador3.Name = "pictureBoxComputador3";
+            this.pictureBoxComputador3.Size = new System.Drawing.Size(160, 103);
+            this.pictureBoxComputador3.TabIndex = 3;
+            this.pictureBoxComputador3.TabStop = false;
+            this.pictureBoxComputador3.Visible = false;
+            // 
+            // pictureBoxNotebook2
+            // 
+            this.pictureBoxNotebook2.Location = new System.Drawing.Point(464, 188);
+            this.pictureBoxNotebook2.Name = "pictureBoxNotebook2";
+            this.pictureBoxNotebook2.Size = new System.Drawing.Size(160, 103);
+            this.pictureBoxNotebook2.TabIndex = 4;
+            this.pictureBoxNotebook2.TabStop = false;
+            this.pictureBoxNotebook2.Visible = false;
+            // 
+            // pictureBoxNotebook3
+            // 
+            this.pictureBoxNotebook3.Location = new System.Drawing.Point(464, 297);
+            this.pictureBoxNotebook3.Name = "pictureBoxNotebook3";
+            this.pictureBoxNotebook3.Size = new System.Drawing.Size(160, 103);
+            this.pictureBoxNotebook3.TabIndex = 5;
+            this.pictureBoxNotebook3.TabStop = false;
+            this.pictureBoxNotebook3.Visible = false;
+            // 
+            // radioButtonComputador
+            // 
+            this.radioButtonComputador.AutoSize = true;
+            this.radioButtonComputador.Location = new System.Drawing.Point(98, 39);
+            this.radioButtonComputador.Name = "radioButtonComputador";
+            this.radioButtonComputador.Size = new System.Drawing.Size(93, 17);
+            this.radioButtonComputador.TabIndex = 6;
+            this.radioButtonComputador.TabStop = true;
+            this.radioButtonComputador.Text = "Computadores";
+            this.radioButtonComputador.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNotebook
+            // 
+            this.radioButtonNotebook.AutoSize = true;
+            this.radioButtonNotebook.Location = new System.Drawing.Point(464, 39);
+            this.radioButtonNotebook.Name = "radioButtonNotebook";
+            this.radioButtonNotebook.Size = new System.Drawing.Size(77, 17);
+            this.radioButtonNotebook.TabIndex = 7;
+            this.radioButtonNotebook.TabStop = true;
+            this.radioButtonNotebook.Text = "Notebooks";
+            this.radioButtonNotebook.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(281, 130);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(91, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Computador 1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(281, 234);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(91, 17);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Computador 2";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(281, 340);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(91, 17);
+            this.radioButton5.TabIndex = 10;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Computador 3";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(647, 130);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(81, 17);
+            this.radioButton6.TabIndex = 11;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Notebook 1";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(647, 234);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(81, 17);
+            this.radioButton7.TabIndex = 12;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Notebook 2";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(647, 340);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(81, 17);
+            this.radioButton8.TabIndex = 13;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Notebook 3";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(266, 415);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpar.TabIndex = 14;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.Location = new System.Drawing.Point(632, 415);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(75, 23);
+            this.buttonSair.TabIndex = 15;
+            this.buttonSair.Text = "Sair";
+            this.buttonSair.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSair);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButtonNotebook);
+            this.Controls.Add(this.radioButtonComputador);
+            this.Controls.Add(this.pictureBoxNotebook3);
+            this.Controls.Add(this.pictureBoxNotebook2);
+            this.Controls.Add(this.pictureBoxComputador3);
+            this.Controls.Add(this.pictureBoxNotebook1);
+            this.Controls.Add(this.pictureBoxComputador1);
+            this.Controls.Add(this.pictureBoxComputador2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxComputador2;
+        private System.Windows.Forms.PictureBox pictureBoxComputador1;
+        private System.Windows.Forms.PictureBox pictureBoxNotebook1;
+        private System.Windows.Forms.PictureBox pictureBoxComputador3;
+        private System.Windows.Forms.PictureBox pictureBoxNotebook2;
+        private System.Windows.Forms.PictureBox pictureBoxNotebook3;
+        private System.Windows.Forms.RadioButton radioButtonComputador;
+        private System.Windows.Forms.RadioButton radioButtonNotebook;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.Button buttonSair;
+    }
+}
+
