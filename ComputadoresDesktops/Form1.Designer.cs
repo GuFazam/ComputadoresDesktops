bed0f8305b9c9a1437968190b64c01624b281685
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-03 20:36:34 -0300

    criei duas labels para exibir o valor de cada computador e notebook, coloquei código em cada radiobutton.

--- ComputadoresDesktops/Form1.Designer.cs
+++ ComputadoresDesktops/Form1.Designer.cs
@@ -45,6 +45,8 @@
             this.radioButton8 = new System.Windows.Forms.RadioButton();
             this.buttonLimpar = new System.Windows.Forms.Button();
             this.buttonSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxComputador1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNotebook1)).BeginInit();
@@ -56,7 +58,7 @@
             // pictureBoxComputador2
             // 
             this.pictureBoxComputador2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxComputador2.Image")));
-            this.pictureBoxComputador2.Location = new System.Drawing.Point(98, 188);
+            this.pictureBoxComputador2.Location = new System.Drawing.Point(98, 169);
             this.pictureBoxComputador2.Name = "pictureBoxComputador2";
             this.pictureBoxComputador2.Size = new System.Drawing.Size(159, 103);
             this.pictureBoxComputador2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -67,7 +69,7 @@
             // pictureBoxComputador1
             // 
             this.pictureBoxComputador1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxComputador1.Image")));
-            this.pictureBoxComputador1.Location = new System.Drawing.Point(98, 83);
+            this.pictureBoxComputador1.Location = new System.Drawing.Point(98, 64);
             this.pictureBoxComputador1.Name = "pictureBoxComputador1";
             this.pictureBoxComputador1.Size = new System.Drawing.Size(159, 99);
             this.pictureBoxComputador1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -78,7 +80,7 @@
             // pictureBoxNotebook1
             // 
             this.pictureBoxNotebook1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNotebook1.Image")));
-            this.pictureBoxNotebook1.Location = new System.Drawing.Point(464, 83);
+            this.pictureBoxNotebook1.Location = new System.Drawing.Point(464, 64);
             this.pictureBoxNotebook1.Name = "pictureBoxNotebook1";
             this.pictureBoxNotebook1.Size = new System.Drawing.Size(160, 99);
             this.pictureBoxNotebook1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -89,7 +91,7 @@
             // pictureBoxComputador3
             // 
             this.pictureBoxComputador3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxComputador3.Image")));
-            this.pictureBoxComputador3.Location = new System.Drawing.Point(98, 297);
+            this.pictureBoxComputador3.Location = new System.Drawing.Point(98, 278);
             this.pictureBoxComputador3.Name = "pictureBoxComputador3";
             this.pictureBoxComputador3.Size = new System.Drawing.Size(160, 103);
             this.pictureBoxComputador3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -100,7 +102,7 @@
             // pictureBoxNotebook2
             // 
             this.pictureBoxNotebook2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNotebook2.Image")));
-            this.pictureBoxNotebook2.Location = new System.Drawing.Point(464, 188);
+            this.pictureBoxNotebook2.Location = new System.Drawing.Point(464, 169);
             this.pictureBoxNotebook2.Name = "pictureBoxNotebook2";
             this.pictureBoxNotebook2.Size = new System.Drawing.Size(160, 103);
             this.pictureBoxNotebook2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -111,7 +113,7 @@
             // pictureBoxNotebook3
             // 
             this.pictureBoxNotebook3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNotebook3.Image")));
-            this.pictureBoxNotebook3.Location = new System.Drawing.Point(464, 297);
+            this.pictureBoxNotebook3.Location = new System.Drawing.Point(464, 278);
             this.pictureBoxNotebook3.Name = "pictureBoxNotebook3";
             this.pictureBoxNotebook3.Size = new System.Drawing.Size(160, 103);
             this.pictureBoxNotebook3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -147,67 +149,79 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(281, 130);
+            this.radioButton3.Location = new System.Drawing.Point(281, 111);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(103, 17);
             this.radioButton3.TabIndex = 8;
             this.radioButton3.Text = "Computador 1";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
             this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(281, 234);
+            this.radioButton4.Location = new System.Drawing.Point(281, 215);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(103, 17);
             this.radioButton4.TabIndex = 9;
             this.radioButton4.Text = "Computador 2";
             this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
             this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton5.Location = new System.Drawing.Point(281, 340);
+            this.radioButton5.Location = new System.Drawing.Point(281, 321);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(103, 17);
             this.radioButton5.TabIndex = 10;
             this.radioButton5.Text = "Computador 3";
             this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Visible = false;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
             // 
             // radioButton6
             // 
             this.radioButton6.AutoSize = true;
             this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton6.Location = new System.Drawing.Point(647, 130);
+            this.radioButton6.Location = new System.Drawing.Point(647, 111);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(91, 17);
             this.radioButton6.TabIndex = 11;
             this.radioButton6.Text = "Notebook 1";
             this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.Visible = false;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
             // 
             // radioButton7
             // 
             this.radioButton7.AutoSize = true;
             this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton7.Location = new System.Drawing.Point(647, 234);
+            this.radioButton7.Location = new System.Drawing.Point(647, 215);
             this.radioButton7.Name = "radioButton7";
             this.radioButton7.Size = new System.Drawing.Size(91, 17);
             this.radioButton7.TabIndex = 12;
             this.radioButton7.Text = "Notebook 2";
             this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.Visible = false;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
             // 
             // radioButton8
             // 
             this.radioButton8.AutoSize = true;
             this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton8.Location = new System.Drawing.Point(647, 340);
+            this.radioButton8.Location = new System.Drawing.Point(647, 321);
             this.radioButton8.Name = "radioButton8";
             this.radioButton8.Size = new System.Drawing.Size(91, 17);
             this.radioButton8.TabIndex = 13;
             this.radioButton8.Text = "Notebook 3";
             this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.Visible = false;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
             // 
             // buttonLimpar
             // 
@@ -231,12 +245,32 @@
             this.buttonSair.UseVisualStyleBackColor = true;
             this.buttonSair.Click += new System.EventHandler(this.buttonSair_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonSair);
             this.Controls.Add(this.buttonLimpar);
             this.Controls.Add(this.radioButton8);
@@ -284,6 +318,8 @@
         private System.Windows.Forms.RadioButton radioButton8;
         private System.Windows.Forms.Button buttonLimpar;
         private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ComputadoresDesktops/Form1.cs
+++ ComputadoresDesktops/Form1.cs
@@ -22,15 +22,29 @@ namespace ComputadoresDesktops
             this.pictureBoxComputador1.Visible = false;
             this.pictureBoxComputador2.Visible = false;
             this.pictureBoxComputador3.Visible = false;
+
             this.pictureBoxNotebook1.Visible = false;
             this.pictureBoxNotebook2.Visible = false;
             this.pictureBoxNotebook3.Visible = false;
+
             this.radioButton3.Checked = false;
             this.radioButton4.Checked = false;
             this.radioButton5.Checked = false;
+
             this.radioButton6.Checked = false;
             this.radioButton7.Checked = false;
             this.radioButton8.Checked = false;
+
+            this.radioButton3.Visible = false;
+            this.radioButton4.Visible = false;
+            this.radioButton5.Visible = false;
+
+            this.radioButton6.Visible = false;
+            this.radioButton7.Visible = false;
+            this.radioButton8.Visible = false;
+
+            this.label1.Visible = false;
+            this.label2.Visible = false;
         }
 
         private void radioButtonComputador_CheckedChanged(object sender, EventArgs e)
@@ -38,15 +52,21 @@ namespace ComputadoresDesktops
             this.pictureBoxComputador1.Visible = true;
             this.pictureBoxComputador2.Visible = true;
             this.pictureBoxComputador3.Visible = true;
+
             this.pictureBoxNotebook1.Visible = false;
             this.pictureBoxNotebook2.Visible = false;
             this.pictureBoxNotebook3.Visible = false;
+
             this.radioButton3.Visible = true;
             this.radioButton4.Visible = true;
             this.radioButton5.Visible = true;
+
             this.radioButton6.Visible = false;
             this.radioButton7.Visible = false;
             this.radioButton8.Visible = false;
+
+            this.label1.Visible = false;
+            this.label2.Visible = false;
         }
 
         private void radioButtonNotebook_CheckedChanged(object sender, EventArgs e)
@@ -54,25 +74,122 @@ namespace ComputadoresDesktops
             this.pictureBoxNotebook1.Visible = true;
             this.pictureBoxNotebook2.Visible = true;
             this.pictureBoxNotebook3.Visible = true;
+
             this.pictureBoxComputador1.Visible = false;
             this.pictureBoxComputador2.Visible = false;
             this.pictureBoxComputador3.Visible = false;
+
             this.radioButton3.Visible = false;
             this.radioButton4.Visible = false;
             this.radioButton5.Visible = false;
+
             this.radioButton6.Visible = true;
             this.radioButton7.Visible = true;
             this.radioButton8.Visible = true;
+
+            this.label1.Visible = false;
+            this.label2.Visible = false;
         }
 
         private void buttonLimpar_Click(object sender, EventArgs e)
         {
+            radioButtonComputador.Checked = false;
+            radioButtonNotebook.Checked = false;
+
             this.radioButton3.Checked = false;
             this.radioButton4.Checked = false;
             this.radioButton5.Checked = false;
+
             this.radioButton6.Checked = false;
             this.radioButton7.Checked = false;
             this.radioButton8.Checked = false;
+
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxComputador1.Visible = true;
+            this.radioButton3.Visible = true;
+
+            this.pictureBoxComputador2.Visible = false;
+            this.pictureBoxComputador3.Visible = false;
+            this.radioButton4.Visible = false;
+            this.radioButton5.Visible = false;
+
+            this.label1.Visible = true;
+            this.label1.Text = "O computador escolhido: Computador 1 Custa R$: 2.000,00";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxComputador2.Visible = true;
+            this.radioButton4.Visible = true;
+
+            this.pictureBoxComputador1.Visible = false;
+            this.pictureBoxComputador3.Visible = false;
+            this.radioButton3.Visible = false;
+            this.radioButton5.Visible = false;
+
+            this.label1.Visible = true;
+            this.label1.Text = "O computador escolhido: Computador 2 Custa R$: 1.500,00";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxComputador3.Visible = true;
+            this.radioButton5.Visible = true;
+
+            this.pictureBoxComputador1.Visible = false;
+            this.pictureBoxComputador2.Visible = false;
+            this.radioButton3.Visible = false;
+            this.radioButton4.Visible = false;
+
+            this.label1.Visible = true;
+            this.label1.Text = "O computador escolhido: Computador 3 Custa R$: 2.500,00";
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxNotebook1.Visible = true;
+            this.radioButton6.Visible = true;
+
+            this.pictureBoxNotebook2.Visible = false;
+            this.pictureBoxNotebook3.Visible = false;
+            this.radioButton7.Visible = false;
+            this.radioButton8.Visible = false;
+
+            this.label2.Visible = true;
+            this.label2.Text = "O Notebook escolhido: Notebook 1 Custa R$: 1.750,00";
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxNotebook2.Visible = true;
+            this.radioButton7.Visible = true;
+
+            this.pictureBoxNotebook1.Visible = false;
+            this.pictureBoxNotebook3.Visible = false;
+            this.radioButton6.Visible = false;
+            this.radioButton8.Visible = false;
+
+            this.label2.Visible = true;
+            this.label2.Text = "O Notebook escolhido: Notebook 2 Custa R$: 2.900,00";
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxNotebook3.Visible = true;
+            this.radioButton8.Visible = true;
+
+            this.pictureBoxNotebook1.Visible = false;
+            this.pictureBoxNotebook2.Visible = false;
+            this.radioButton6.Visible = false;
+            this.radioButton7.Visible = false;
+
+            this.label2.Visible = true;
+            this.label2.Text = "O Notebook escolhido: Notebook 3 Custa R$: 1.550,00";
         }
     }
-}
+    }
